51c353820228bd4b0e1a9b3708569376cee3eb17
Erik Schouten <Erik@ErikSchouten.com>
2018-06-18 16:45:58 +0200

    return primes & testing

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,67 @@
+import java.util.List;
+import java.util.StringJoiner;
 import java.util.concurrent.ExecutionException;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
-        int threads = 2;
+    public static void main(String[] args) {
+
+        test(4);
+
+        int threads = 4;
         int max = 1000;
-//        Manager manager = new Manager(1000000000, 3, true);
-//        DistributableWorker worker = new DistributableWorker();
-//        worker.main(new String[0]);
-        Manager manager = new Manager(max, threads, true);
-        manager.start();
+        int tries = 6;
+
+        for (int j = 0; j < tries; j++) {
+            for (int i = 0; i < threads; i++) {
+                new Thread(() -> {
+                    DistributableWorker worker = new DistributableWorker();
+                    try {
+                        worker.main(new String[0]);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }).start();
+            }
+
+            Manager manager = new Manager(max, threads, false);
+            manager.start();
+        }
+    }
+
+    private static void test(int threads) {
+
+        for (int i = 0; i < threads; i++) {
+            new Thread(() -> {
+                DistributableWorker worker = new DistributableWorker();
+                try {
+                    worker.main(new String[0]);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }).start();
+        }
+
+        Manager manager = new Manager(1000, threads, false);
+
+        List<Integer> primes = null;
+
+        try {
+            primes = manager.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        StringJoiner sj = new StringJoiner(", ");
+        for (Integer prime: primes) {
+            sj.add(prime.toString());
+        }
+        String primeList = sj.toString();
+        String allPrimesTill1000 = "2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997";
+        if (primeList.equals(allPrimesTill1000)) {
+            System.out.println("TEST SUCCEEDED!");
+        } else {
+            System.out.println("TEST FAILED!");
+        }
     }
 }
